ea29c12419eec13259f28e867d42fada686296b1
Camila-Cano <mariacano9011@gmail.com>
2025-03-25 18:40:48 -0500

    init proyecto frontend

---
+++ src/app/registro-empleado/registro-empleado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-registro-empleado',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './registro-empleado.component.html',
+  styleUrl: './registro-empleado.component.css'
+})
+export class RegistroEmpleadoComponent {
+
+}
