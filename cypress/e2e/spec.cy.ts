338f6358c63b820a528a0499200e7a14f353a8c5
Camila-Cano <mariacano9011@gmail.com>
2025-05-04 19:45:26 -0500

    implementacion de pruebas automaticas con cypress

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,25 @@
+describe('Automatizacon caso de prueba para registrar un empleado', () => {
+  it('Registro exitoso de un empleado', () => {
+    // apertura del sitio y seleccion de opcion de menu
+    cy.visit('/');
+    cy.get('#menuRegistroEmpleado').click();
+    // diligenciamiento del formulario
+    let identificacion = Math.floor(Math.random() * 9999999999) + 1000000;
+    let codigo = Math.floor(Math.random() * 999) + 100;
+    cy.get('#documentoIdentidad').type(identificacion.toString());
+    cy.get('#codigo').type(codigo.toString());
+    cy.get('#nombre').type('Maria Camila');
+    cy.get('#apellido').type('Cano Chica');
+    cy.get('#direccion').type('Calle 33 # 28 - 44');
+    cy.get('#telefono').type('3177577212');
+    // ejecucion de la solicitud de guardado
+    cy.get('#btnCrearEmpleado').click();
+    cy.wait(3000);
+    // validacion de prueba
+    cy.get('.ng-star-inserted').first().then((element) => {
+      let textValue = Cypress.$(element).text().trim()
+      expect(textValue).to.equal('SuccessOperacion ejecutada con exito OK')
+    });
+  });
+
+});
